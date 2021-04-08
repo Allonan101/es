2185bc805529e3218032327ca69579fa6fc6da35
wilson agrela <wilson-andre100@hotmail.com>
2021-04-08 17:27:57 +0100

    Menu inicial adicionado

--- Trabalho_1_adriano_wilson/Trabalho_1_adriano_wilson/Program.cs
+++ Trabalho_1_adriano_wilson/Trabalho_1_adriano_wilson/Program.cs
@@ -6,7 +6,42 @@ namespace Trabalho_1_adriano_wilson
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool repetir = true;
+            while (repetir == true)
+            {
+                Console.WriteLine("Bem vindo a clinica veterinaria");
+                Console.WriteLine("1- Registe um animal");
+                Console.WriteLine("2- Registe um cliente");
+                Console.WriteLine("3- Serviços");
+                Console.WriteLine("4- Fazer o relatório");
+                Console.WriteLine("5- Sair");
+                Console.Write("=>");
+                int opcao = Convert.ToInt32(Console.ReadLine());
+           
+                switch (opcao)
+                {
+                    case 1:
+                        Console.WriteLine("Registou um animal");
+                        break;
+                    case 2:
+                        Console.WriteLine("Registou um cliente");
+                        break;
+                    case 3:
+                        Console.WriteLine("Mostra todos os serviços disponiveis");
+                        Console.WriteLine("Mostra depois todos os empregados disponiveis para ajudar");
+                        break;
+                    case 4:
+                        Console.WriteLine("Faz relatorio sobre todas as informações");
+                        break;
+                    case 5:
+                        repetir = false;
+                        break;
+                    default:
+                        Console.WriteLine("default case");
+                        break;
+
+                }
+            }
         }
     }
 }
