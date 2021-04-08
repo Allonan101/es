c36cf169a915ca822d9ddf12b9d1eadaa6499af8
Wilson Agrela <wilson-andre100@hotmail.com>
2021-04-08 14:52:25 +0100

    Add files via upload

---
+++ Trabalho_1_adriano_wilson/Trabalho_1_adriano_wilson/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Trabalho_1_adriano_wilson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
