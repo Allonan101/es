317a5892783c629d4297eb1029b7953427c85dfa
wilson agrela <wilson-andre100@hotmail.com>
2021-04-08 15:39:32 +0100

    classes adicionadas

---
+++ Trabalho_1_adriano_wilson/Trabalho_1_adriano_wilson/Pessoa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trabalho_1_adriano_wilson
+{
+    class Pessoa
+    {
+        private string nome;
+        private int contacto;
+        private string endereco;
+        
+    }
+}
